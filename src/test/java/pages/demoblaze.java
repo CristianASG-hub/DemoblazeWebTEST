719654ced558225ca41e57c77a98a2186c8acbac
Cristian Suarez <cris_cuq@hotmail.com>
2025-02-10 00:33:25 -0300

    Update

---
+++ src/test/java/pages/demoblaze.java
@@ -0,0 +1,39 @@
+package pages;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+
+
+
+
+public class demoblaze {
+    
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void  setUp()
+    { 
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test 
+    public void goToDemoblaze(){
+                         
+        driver.get("https://demoblaze.com/");
+    }
+      
+    @AfterMethod
+    public void tearDown(){
+    
+        if(driver != null){
+            driver.quit();
+        }
+    }
+
+}
